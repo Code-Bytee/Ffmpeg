00270410e3ce071c34c81dd87fbdc703944c0144
Anshul <72375357+Code-Bytee@users.noreply.github.com>
2023-06-05 13:23:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import os
 import requests
 
-url = 'https://srv.badasserver3.com/download?data=xyJzMz3ZmdBUJo5spb262dgGJC3EbDaDbC%2FmLZl3xWukgDSjGPHNhr5cX3Kt%2FTgpBPHUDfqvx34ez92cpXq0in4KSu%2Fh0uTfBYqjY0f%2FMnupfqE1Xoy%2FeCO4d71xzjo0SxdDKopTarqZOrGZ3gqN19jsTs5HgKMg3buhjJMEstzYeQLyiSEBIJk4QukStFlBUl6fEKrrqnjVrzUA2YBPEsf4s4xdyPlu3XA1ZZNtTdVJg%2FTgNzKoxqEqKBlD5kZ%2FiFzskY%2FgKvgOut7x3hZaW7ZC8zyvJmTo80ZeKOVIbVdYvXxvohC7rCyq%2FDycu3PKskbeAqhl6ZYJYQFZ%2BQH8OPhwr70QdnCLCDO5n%2Ftkjyd7Tfm8dzXI200KOk8ysyvuaug8l27UC%2FwkCuaeEQEimysT%2FDrjwzWaBqoiCP%2FAJ6uDkvQn%2FEt6fmp%2BOkfW%2BxBNQo%2FVobW%2FCF4VES6IZcL6Gw%3D%3D'                                                                 
+
+url = 'https://pt01001.cdntrex.com/remote_control.php?time=1685951554&cv=5238fee3ead285ee0e0e59b458c44a71&lr=0&cv2=64a100c1bb0d5919fcdcad4626756bc1&file=%2F2277000%2F2277506%2F2277506_1080p.mp4&cv3=693e15b43e131045e0ebfb940709ed60&cv4=ded06edc116baa4fd6762c9d75e720bc'
+
+
 os.system("curl --output tmp.mp4 "+url)
 os.system("ffmpeg -i tmp.mp4 -ss 1:30 -t 30 ./vid/clip.mp4")
 os.system("ffmpeg -y -i './vid/clip.mp4' -c:v libx265 -q:v 5 -c:a aac -a b 128k -ar 44100 './vid/final.mp4'")
