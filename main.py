07fc5152b36a446cbf278d40ae3f3cd154a114b9
Anshul <72375357+Code-Bytee@users.noreply.github.com>
2023-06-05 14:28:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ with open('tmp.mp4','wb') as f:
   f.write(requests.get(url).content)
 #os.system("curl --output tmp.mp4 "+url)
 os.system("ffmpeg -i tmp.mp4 -ss 1:30 -t 30 clp.mp4")
-os.system("ffmpeg -y -i 'clp.mp4' -c:v libx265 -q:v 5 -c:a aac -a b 128k -ar 44100 'final.mp4'")
+os.system("ffmpeg -y -i 'clp.mp4' -c:v libx265 'final.mp4'")
 os.system("mv final.mp4 ./vid/")
 
 #os.system("ffmpeg -y -i 'tmp.mp4' -c:v libx265 -q:v 5 -c:a aac -ab 128k -ar 44100 'final.mp4'")
