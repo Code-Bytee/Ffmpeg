1e2149e985856ebe0240e868b724aea09556acac
Anshul <72375357+Code-Bytee@users.noreply.github.com>
2023-06-05 14:43:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import os
 import requests
 
-url = 'https://vdownload-33.sb-cd.com/1/0/10448931-1080p.mp4?secure=Knb7OMx4w6tcZXwOCNqtGw,1685971528&m=33&d=5&_tid=10448931&name=Dani_Daniels_Seduces_Cameraman_in_Hotel_Fuckfest-pussyspace.com.mp4'
 
+url = 'https://vdownload-42.sb-cd.com/1/3/13187491-1080p.mp4?secure=CY3p74WfKFBZFDBjepQsCQ,1685996303&m=42&d=1&_tid=13187491&name=She_reminds_me_of_Dani_Daniels-pussyspace.com.mp4'
 with open('tmp.mp4','wb') as f:
   f.write(requests.get(url).content)
 #os.system("curl --output tmp.mp4 "+url)
-os.system("ffmpeg -i tmp.mp4 -ss 1:30 -t 30 clp.mp4")
-os.system("ffmpeg -y -i 'clp.mp4' -c:v libx265 'final.mp4'")
+#os.system("ffmpeg -i tmp.mp4 -ss 1:30 -t 30 clp.mp4")
+os.system("ffmpeg -y -i 'tmp.mp4' -c:v libx265 'final.mp4'")
 os.system("mv final.mp4 ./vid/")
 
 #os.system("ffmpeg -y -i 'tmp.mp4' -c:v libx265 -q:v 5 -c:a aac -ab 128k -ar 44100 'final.mp4'")
